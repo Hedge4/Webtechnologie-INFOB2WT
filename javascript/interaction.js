69c8f2bb5faf441a56972575898e9bf334237630
Hedde van Heerde <hedde.vanheerde@verbumvolat.nl>
2023-03-16 22:39:24 +0100

    Moved cast.html specific code to cast.js file

--- javascript/interaction.js
+++ javascript/interaction.js
@@ -13,13 +13,7 @@ const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matche
   } */
 
 
-  const images = document.querySelectorAll('.cast-member');
+/////////////////////////
+//  SHOW/HIDE SIDEBAR  //
+/////////////////////////
 
-  images.forEach(function(image) {
-    image.addEventListener('mouseover', function() {
-      this.style.transform = 'scale(1.1)';
-    });
-    image.addEventListener('mouseout', function() {
-      this.style.transform = 'scale(1)';
-    });
-  });
